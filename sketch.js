b5e1966772bf69f0209e5c57dcef018e1a3cd139
rdhimal007 <73763017+rdhimal007@users.noreply.github.com>
2022-10-19 01:19:38 -0500

    "making a border side wall"

--- sketch.js
+++ sketch.js
@@ -4,8 +4,15 @@
 function setup(){
     // making a 720p resolution
     createCanvas(1280,720);
-    
+
+
 }
 function draw(){
     background("blue");
+
+    // create a blue border wall
+    fill("white");
+    rect(12, 10, 1220, 700);
+    noFill();
+
 }
