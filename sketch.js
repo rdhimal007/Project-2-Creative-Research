9f236bb131f3335f48d07e08327790d0c0e4cd98
rdhimal007 <73763017+rdhimal007@users.noreply.github.com>
2022-10-24 23:04:20 -0500

    adding the new file

--- sketch.js
+++ sketch.js
@@ -1,18 +1,26 @@
 // Name : Roshan Dhimal
 // Project2: Creative Research
 
+
+// create image
+let img;
+// preload the image
+function preload() {
+    img = loadImage('temp1.jpg');
+}
+fu
 function setup(){
     // making a 720p resolution
-    createCanvas(1280,720);
+    createCanvas(1290,720);
 
 
 }
 function draw(){
     background("blue");
-
     // create a blue border wall
     fill("white");
-    rect(12, 10, 1220, 700);
-    noFill();
+    rect(12, 10, 1265, 700);
 
+    noFill();
+    image(img,320,200);
 }
